5b19cee4b3ab3bc995d942b7cd06670a71d69740
angelbarrera92 <angelbarrerasanchez@protonmail.com>
2022-10-22 13:43:13 +0200

    Add version information to the CLI

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -9,12 +10,33 @@ import (
 	"github.com/angelbarrera92/tlwpa4220/cmd/serve"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 type CliCommand interface {
 	Parse(args []string) error
 	Run() error
 }
 
+func printHelp() {
+	fmt.Println("Usage: tlwpa4220 [command]")
+	fmt.Println("Available commands:")
+	fmt.Println("  serve-metrics")
+	fmt.Println("  cli")
+	// Print also the version
+	fmt.Printf("Version: %s, commit: %s, built at: %s\n", version, commit, date)
+}
+
 func main() {
+
+	// Print help if no arguments are provided
+	if len(os.Args) == 1 {
+		printHelp()
+		os.Exit(0)
+	}
 
 	if len(os.Args) < 2 {
 		log.Println("serve-metrics or cli subcommand is required")
